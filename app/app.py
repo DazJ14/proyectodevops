0693e8915873587d38567abc92dd08cb4de1c91e
Diego Zamora <jaguares_diego@hotmail.com>
2026-05-10 11:37:21 -0600

    ???

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "Todo parece ir bien... Demasiado bien."
+    return "Jordi, Chris, en serio vayanse directito a la... los tqm, ah y corriendo en puerto 5000"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
